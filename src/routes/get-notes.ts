40ec3b098d36b8592fc4efb165123129363600d0
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-05 19:48:06 -0300

    feat: create get notes service and route

--- src/routes/get-notes.ts
+++ src/routes/get-notes.ts
@@ -1,5 +1,7 @@
+import { authorizeMiddleware } from "../middlewares/authorize-middleware";
 import { PrismaService } from "../repositories/prisma/prisma-service";
 import { GetNotesService } from "../services/get-notes";
+import { GetStudentBySessionIdService } from "../services/get-student-by-sessionId";
 import { FastifyTypedInstance } from "../types";
 import { z } from 'zod';
 
@@ -7,21 +9,27 @@ export async function GetNotesRoute(app: FastifyTypedInstance){
   app.get('/notes', 
     {
       schema: {
-        response: {
-          200: z.array(z.object({
-            title: z.string(),
-            content: z.string(),
-            isPrivate: z.boolean(),
-            createdAt: z.date(),
-          }))
-        }
-      }
+        // response: {
+        //   200: z.array(z.object({
+        //     title: z.string(),
+        //     content: z.string(),
+        //     isPrivate: z.boolean(),
+        //     createdAt: z.date(),
+        //   }))
+        // }
+      },
+      preHandler: [authorizeMiddleware]
     },async (req, res) =>{
       const prismaRepository = new PrismaService();
-      // after login implementation, get user cookies
-      const getNotesService = new GetNotesService(prismaRepository);
+      
+      const { sessionId } = req.cookies;
+      if(!sessionId) throw new Error()
 
-      // const notes = await getNotesService.execute('eb054b69-3201-48e5-be7d-b651a6609429')\
-      // return res.status(200).send(notes)
+      const getNotesService = new GetNotesService(prismaRepository);
+      const getStudentBySessionIdService = new GetStudentBySessionIdService(prismaRepository)
+      const student = await getStudentBySessionIdService.execute(sessionId)
+
+      const notes = await getNotesService.execute(student.id)
+      return res.status(200).send(notes)
   })
 }
