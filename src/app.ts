832a9c4a656e710c4804bc7cc3e8fac33464df1e
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-05 19:50:05 -0300

    feat: set jwt configurarion and including all routes on app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import { fastify } from 'fastify'
 import cookie from '@fastify/cookie'
+import jwt from '@fastify/jwt'
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 import { RegisterStudentRoute } from './routes/register-student';
 import { RegisterNoteRoute } from './routes/register-notes';
 import { DeleteNoteRoute } from './routes/delete-note';
 import { GetNotesRoute } from './routes/get-notes';
+import { AddFriendRoute } from './routes/add-friend';
+import { BlockFriendRoute } from './routes/block-friend';
+import { GetFriendNotesRoute } from './routes/get-friend-notes';
+import { LoginStudentRoute } from './routes/login-student';
+import { GetFriendsRoute } from './routes/get-friends';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler)
@@ -15,9 +21,23 @@ app.register(cookie, {
     hook: 'onRequest'
 })
 
+app.register(jwt, {
+    secret: process.env.COOKIE_SECRET!
+})
+
+app.addHook('preHandler', (req, res, done) =>{
+    req.jwt = app.jwt;
+    return done();
+})
+
 app.register(RegisterStudentRoute)
 app.register(RegisterNoteRoute)
 app.register(DeleteNoteRoute)
-// app.register(GetNotesRoute)
+app.register(GetNotesRoute)
+app.register(AddFriendRoute)
+app.register(BlockFriendRoute)
+app.register(GetFriendNotesRoute)
+app.register(LoginStudentRoute)
+app.register(GetFriendsRoute)
 
 export { app };
