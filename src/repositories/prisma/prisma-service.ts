e66828d5945589780103d8446fed0aa07e557828
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-30 12:07:31 -0300

    feat: change database url for environment and production dynamically

--- src/repositories/prisma/prisma-service.ts
+++ src/repositories/prisma/prisma-service.ts
@@ -2,6 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 export class PrismaService extends PrismaClient{
     constructor(){
-        super();
+        super({
+          datasources: {
+            db: {
+              url: process.env.NODE_ENV === "production" ? process.env.DATABASE_URL : "postgresql://admin:admin@localhost:5432/mypostgres?schema=public"
+            }
+          }
+        });
     }
 }
